6dcd64522fd4ae499a272ec25b9e6e335770f6b5
Enrique Galeano <enrique.galeano99@unitec.edu>
2017-09-15 22:49:51 -0600

    Empezamos de nuevo

---
+++ Proyecto_Programacion2_EnriqueGaleano/src/AdministrarPersona.java
@@ -0,0 +1,89 @@
+
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.ArrayList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author enriquejosegaleanotalavera
+ */
+public class AdministrarPersona implements Serializable{
+	
+	public final long SerializableUID = 555L;
+	
+	private ArrayList<usuario> persona = null;
+	private File pathLocation = null;
+
+	public AdministrarPersona(String path){
+		pathLocation = new File(path);
+	}
+
+	public ArrayList<usuario> getListaDeUsuario() {
+		return persona;
+	}
+
+	public void setListaDeUsuario(ArrayList<usuario> p) {
+		this.persona = persona;
+	}
+
+	public File getPathLocation() {
+		return pathLocation;
+	}
+
+	public void setPathLocation(File pathLocation) {
+		this.pathLocation = pathLocation;
+	}
+
+	public void cargarArchivo() {
+		try {
+			persona = new ArrayList();
+			usuario temp;
+			if (pathLocation.exists()) {
+				FileInputStream entrada = new FileInputStream(pathLocation);
+				ObjectInputStream objeto = new ObjectInputStream(entrada);
+				try {
+					while ((temp = (usuario) objeto.readObject()) != null) {
+						persona.add(temp);
+					}
+				} catch (EOFException e) {
+				}
+				objeto.close();
+				entrada.close();
+			}//End if
+		} catch (Exception e) {
+		}
+	}
+
+	public void escribirArchivo() {
+		FileOutputStream fw = null;
+		ObjectOutputStream bw = null;
+		try {
+			fw = new FileOutputStream(pathLocation);
+			bw = new ObjectOutputStream(fw);
+			for (usuario t : persona) {
+				bw.writeObject(t);
+			}
+			bw.flush();
+		} catch (Exception e) {
+
+		} finally {
+			try {
+				bw.close();
+				fw.close();
+			} catch (Exception e) {
+			}
+		}
+	}
+
+}
---
+++ Proyecto_Programacion2_EnriqueGaleano/src/usuario.java
@@ -0,0 +1,88 @@
+
+import java.util.ArrayList;
+import java.util.Date;
+import javax.swing.Icon;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author enriquejosegaleanotalavera
+ */
+public class usuario {
+	public final long SerializableUID = 333L;
+	private String nombreCompleto;
+	private String nickname;
+	private String password;
+	private String pais;
+	private Date fechaDeNacimiento;
+	private Icon fotografia;
+
+
+	public usuario(String nombreCompleto, String nickname, String password, String pais, Date fechaDeNacimiento, Icon fotografia) {
+		this.nombreCompleto = nombreCompleto;
+		this.nickname = nickname;
+		this.password = password;
+		this.pais = pais;
+		this.fechaDeNacimiento = fechaDeNacimiento;
+		this.fotografia = fotografia;
+	}
+
+	public Icon getFotografia() {
+		return fotografia;
+	}
+
+	public void setFotografia(Icon fotografia) {
+		this.fotografia = fotografia;
+	}
+
+	public String getNombreCompleto() {
+		return nombreCompleto;
+	}
+
+	public void setNombreCompleto(String nombreCompleto) {
+		this.nombreCompleto = nombreCompleto;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPais() {
+		return pais;
+	}
+
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+
+	public Date getFechaDeNacimiento() {
+		return fechaDeNacimiento;
+	}
+
+	public void setFechaDeNacimiento(Date fechaDeNacimiento) {
+		this.fechaDeNacimiento = fechaDeNacimiento;
+	}
+
+	@Override
+	public String toString() {
+		return nombreCompleto;
+	}
+	
+}
